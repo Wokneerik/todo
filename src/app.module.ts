c7e1f1838171e114b6926be90f2dd7705c99fdec
Wokneerik <maximkireenkov@gmail.com>
2023-04-07 16:18:29 +0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,28 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { TelegrafModule } from 'nestjs-telegraf'
-import { join } from 'path'
-import * as LocalSession from 'telegraf-session-local'
 import { AppService } from './app.service'
 import { AppUpdate } from './app.update'
 import { TaskEntity } from './task.entity'
 
-const sessions = new LocalSession({database: 'session_db.json'})
+// const sessions = new LocalSession({database: 'session_db.json'})
 
 @Module({
   imports: [
     TelegrafModule.forRoot({
-      middlewares: [sessions.middleware()],
+      // middlewares: [sessions.middleware()],
       token: String(process.env.BOT_TOKEN) 
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'ec2-34-241-82-91.eu-west-1.compute.amazonaws.com',
-      port: 5432,
-      database: 'dru7mog8r16oh',
-      username: 'hgoqyvrofzuuxm',
-      password: '698a14e410139528f2f9c284e7e3e1c122224df2502abe8c08ea61069e3fb18b',
-      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      type: 'postgres',
+      // host: 'ec2-34-241-82-91.eu-west-1.compute.amazonaws.com',
+      // port: 5432,
+      // database: 'dru7mog8r16oh',
+      // username: 'hgoqyvrofzuuxm',
+      // password: '698a14e410139528f2f9c284e7e3e1c122224df2502abe8c08ea61069e3fb18b',
+      url: "postgres://hgoqyvrofzuuxm:698a14e410139528f2f9c284e7e3e1c122224df2502abe8c08ea61069e3fb18b@ec2-34-241-82-91.eu-west-1.compute.amazonaws.com:5432/dru7mog8r16oh",
+      logging: true,
+      entities: ["src/entity/*.*"],
       synchronize: true,
       dropSchema: true
     }),
